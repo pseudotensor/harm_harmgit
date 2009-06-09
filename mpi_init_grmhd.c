d7638b693025656dc431ecb0793ec410b5a31074
Jonathan C. McKinney <pseudotensor@gmail.com>
2009-06-09 19:27:28 +0000

    Very minor [not even used yet] update to setting MPIid array

--- mpi_init_grmhd.c
+++ mpi_init_grmhd.c
@@ -224,10 +224,11 @@ int init_MPI_GRMHD_myid(void)
   //  truempiid=MPIid[myid];
 
 
-  // Call user function
+  // Call user function [can have myid==0 setup all CPUs or just have all CPUs do same setup]
   theproblem_set_myid();
 
-  
+  // Might have myid==0 setup al CPUs, but not MPIid[0] since unknown by all CPUs at first and might change.  So use myid==0 as broadcast in case myid==0 setup all CPUs.
+  MPI_Bcast(MPIid,MAXCPUS,MPI_INT,myid,MPI_COMM_GRMHD);
 
   return(0);
   
