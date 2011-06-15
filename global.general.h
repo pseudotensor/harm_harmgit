01ce60ad004d0f87db54ade689c3629da86bdbed
Jonathan C. McKinney <pseudotensor@gmail.com>
2011-06-14 20:20:16 -0700

    minor

--- global.general.h
+++ global.general.h
@@ -124,7 +124,7 @@
 // comment below 3 lines when not wanting to test
 // uncomment when wanting to test, and then look at make.log
 //
-#if(1)
+#if(0)
 #if(!defined(doublereal))
 typedef double doublereal;
 #endif
