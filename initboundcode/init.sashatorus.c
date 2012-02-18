a5b5525c997133b634191a91aedee564050a9ccb
Alexander Tchekhovskoy <atchekho@princeton.edu>
2012-02-17 22:08:30 -0500

    bsq/rho = 50, bsq/u = 500, no rotation

--- initboundcode/init.sashatorus.c
+++ initboundcode/init.sashatorus.c
@@ -208,7 +208,7 @@ int post_init_specific_init(void)
   t_transition = 1.;
   global_vpar0 = 0.;
   
-  BSQORHOLIMIT=5*1E2; // was 1E2 but latest BC test had 1E3 // CHANGINGMARK
+  BSQORHOLIMIT=0.5*1E2; // was 1E2 but latest BC test had 1E3 // CHANGINGMARK
   BSQOULIMIT=5*1E2; // was 1E3 but latest BC test had 1E4
   UORHOLIMIT=5*1E2;
 
@@ -526,9 +526,9 @@ int init_global(void)
   //  rescaletype=1;
   rescaletype=4;
   //SASMARK: decrease magnetization by 2x to make it easier (still is around ~45>>1)
-  BSQORHOLIMIT=0.1*5*1E2; // was 1E2 but latest BC test had 1E3 // CHANGINGMARK
-  BSQOULIMIT=0.1*1*1E2; // was 1E3 but latest BC test had 1E4
-  UORHOLIMIT=0.1*1*1E2;
+  BSQORHOLIMIT=0.5*1E2; // was 1E2 but latest BC test had 1E3 // CHANGINGMARK
+  BSQOULIMIT=5*1E2; // was 1E3 but latest BC test had 1E4
+  UORHOLIMIT=5*1E2;
   RHOMIN = 1E-4;
   UUMIN = 1E-4;
 #elif(WHICHPROBLEM==GRBJET)
@@ -642,9 +642,9 @@ int init_grid_post_set_grid(FTYPE (*prim)[NSTORE2][NSTORE3][NPR], FTYPE (*pstag)
   FTYPE X[NDIM],V[NDIM],r,th;
   extern void check_spc_singularities_user(void);
 
-  BSQORHOLIMIT=0.1*5*1E2; // was 1E2 but latest BC test had 1E3 // CHANGINGMARK
-  BSQOULIMIT=0.1*1*1E2; // was 1E3 but latest BC test had 1E4
-  UORHOLIMIT=0.1*1*1E2;
+  BSQORHOLIMIT=0.5E2; // was 1E2 but latest BC test had 1E3 // CHANGINGMARK
+  BSQOULIMIT=5*1E2; // was 1E3 but latest BC test had 1E4
+  UORHOLIMIT=5*1E2;
 
   // some calculations, althogh perhaps calculated already, definitely need to make sure computed
 #if(MCOORD==KSCOORDS)
