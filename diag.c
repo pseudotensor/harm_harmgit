1f339c0747664f13c64ce7c412afcd461e8dc474
Sasha <atchekho@princeton.edu>
2012-05-01 15:32:20 -0400

    remove finalstepbackup's from diag.c

--- diag.c
+++ diag.c
@@ -42,7 +42,6 @@ int diag(int call_code, FTYPE localt, long localnstep, long localrealnstep)
   int asym_compute_1(FTYPE (*prim)[NSTORE2][NSTORE3][NPR]);
   int whichDT;
   int (*dumpfuncgen[NUMDUMPTYPES])(long dump_cnt);
-  int finalstepbackup;
 
   
 
@@ -204,8 +203,6 @@ int diag(int call_code, FTYPE localt, long localnstep, long localrealnstep)
      || (PRODUCTION>0 && (dodumpgen[MAINDUMPTYPE]))
      ){
 
-    finalstepbackup = finalstepglobal;
-    finalstepglobal = 0;  //only for diagnostic, no accounting
     // for dump, rdump, and divb in ener
     int finalstep=0; // only for diagnostics, no accounting.
     bound_allprim(STAGEM1,finalstep, localt,GLOBALPOINT(pdump),GLOBALPOINT(pstagdump),GLOBALPOINT(udump), USEMPI);
@@ -214,7 +211,6 @@ int diag(int call_code, FTYPE localt, long localnstep, long localrealnstep)
       // Notice only need to bound 1 cell layer (BOUNDPRIMSIMPLETYPE) since divb computation only will need 1 extra cell
       bound_mpi(STAGEM1,finalstep,BOUNDPRIMSIMPLETYPE,GLOBALPOINT(udump),NULL,NULL,NULL,NULL);
     }
-    finalstepglobal = finalstepbackup;
   }
   
 
