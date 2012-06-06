cd111a18631ed5e70f612bcfef6c428d1cbc9b38
Alexander Tchekhovskoy <atchekho@princeton.edu>
2012-06-06 11:42:43 -0400

    Expanded set emf loop to include outer polar axis, hopefully should avoid waves

--- bounds.tools.c
+++ bounds.tools.c
@@ -3478,8 +3478,7 @@ void user1_adjust_fluxctstag_emfs(SFTYPE time, FTYPE (*prim)[NSTORE2][NSTORE3][N
 	COMPFULLLOOPP1_23{
 	  // EMF[2]:
 #if(N3>1 && DONSEMFS)
-	  if( j >= 0 && j < N2 && k >= 0 && k <= N3 ){
-	    get_geometry(i, j, k  , FACE1, ptrgeom); //CORN2 -- "corner" in 1-3 	    //get_geometry(i, j, k  , CORN2, ptrgeom_ph1); //CORN2 -- "corner" in 1-3 plane, think this is where E_theta is   located
+	  if( j >= 0 && j <= N2 && k >= 0 && k <= N3 ){
 	    //get_geometry(i, j, k+1, CORN2, ptrgeom_ph2); //CORN2 -- "corner" in 1-3 plane, think this is where E_theta is located
 	    bl_coord_ijk(i, j, k  , CORN2, V_ph1);
 	    bl_coord_ijk(i, j, k+1, CORN2, V_ph2);
@@ -3490,20 +3489,21 @@ void user1_adjust_fluxctstag_emfs(SFTYPE time, FTYPE (*prim)[NSTORE2][NSTORE3][N
 	    //km1 = km1mac(k);
 	    //km1 = max(km1, INFULL3);
 	    omega = get_omegaf_phys(t, dt, steppart);
-	    aflux = vpotns_flux(V_ph1[1],V_th1[2],V_th2[2],V_ph1[3]-omega*t,V_ph2[3]-omega*t);
-	    nflux = ptrgeom->gdet * GLOBALMACP0A1(pstagglobal,i,j,k,B1)*dx[2]*dx[3]; //MACP0A1(prim,i,j,k,B1)
 	    dflux = dfluxns(V_ph1[1], omega, V_ph1[3], V_th1[2], V_th2[2], t, dt);
 	    //myB1 = 0.5 * ( GLOBALMACP0A1(pstagglobal,i,j,k,B1)+GLOBALMACP0A1(pstagglobal,i,j,km1,B1) );
 	    //d(gdet*B1)/dt = -dF3(B1)/dx3
 	    //dflux = d(gdet*B1*dx2*dx3) = -dF3(B1)*dx2*dt  
 	    //F3(B1) = dflux / (dx2*dt) <-- make sure sign correct
 #if(0)
-	    if( 1 || j == 1 && k == 1 ){
-		dualfprintf(fail_file, "t = %21.15g, nstep = %ld, sp = %d, fluxvec[3][%d][%d][%d][B1] = %g, emf = %g, aflux = %g, nflux = %g\n",
-			    t, nstep, steppart, i, j, k, MACP1A1(fluxvec,3,i,j,k,B1), dflux / (dx[2] * dt),
-			    aflux, nflux);
-		//dualfprintf(fail_file,"r=%g, th1=%g, th2=%g, ph1=%g, ph2=%g\n", V_ph1[1], V_th1[2], V_th2[2], V_ph1[3], V_ph2[3]);
-	      
+	    if( 1 || j == 1 && k == 1 ){  
+	      get_geometry(i, j, k  , FACE1, ptrgeom); //CORN2 -- "corner" in 1-3 	    //get_geometry(i, j, k  , CORN2, ptrgeom_ph1); //CORN2 -- "corner" in 1-3 plane, think this is where E_theta  is   located
+	      aflux = vpotns_flux(V_ph1[1],V_th1[2],V_th2[2],V_ph1[3]-omega*t,V_ph2[3]-omega*t);
+	      nflux = ptrgeom->gdet * GLOBALMACP0A1(pstagglobal,i,j,k,B1)*dx[2]*dx[3]; //MACP0A1(prim,i,j,k,B1)
+	      dualfprintf(fail_file, "t = %21.15g, nstep = %ld, sp = %d, fluxvec[3][%d][%d][%d][B1] = %g, emf = %g, aflux = %g, nflux = %g\n",
+			  t, nstep, steppart, i, j, k, MACP1A1(fluxvec,3,i,j,k,B1), dflux / (dx[2] * dt),
+			  aflux, nflux);
+	      //dualfprintf(fail_file,"r=%g, th1=%g, th2=%g, ph1=%g, ph2=%g\n", V_ph1[1], V_th1[2], V_th2[2], V_ph1[3], V_ph2[3]);
+	    
 	    }
 #endif
 	    MACP1A1(fluxvec,3,i,j,k,B1) = dflux / (dx[2] * dt);   // rotation, E_2 = (-[v x B])_2 = - v^3 B^1
