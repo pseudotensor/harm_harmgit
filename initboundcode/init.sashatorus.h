2dd6ede4d3952aa43d71ae59ee530a29ab6c0d11
Alexander Tchekhovskoy <atchekho@princeton.edu>
2012-08-08 18:08:36 -0400

    FULLOUTPUT 0

--- initboundcode/init.sashatorus.h
+++ initboundcode/init.sashatorus.h
@@ -181,8 +181,8 @@
 #define HIGHERORDERMEM 0
 #define MAXBND 4 // 4 for PARAFLAT, 6 for WENO5BND wo/a2c stuff : 11 for full point-field FLUXRECON method
 #define PRODUCTION 1
-#define FULLOUTPUT MAXBND
-//#define FULLOUTPUT 0
+//#define FULLOUTPUT MAXBND
+#define FULLOUTPUT 0
 
 #define MAILWHENDONE 0
 #define MAILFROMREMOTE 0
