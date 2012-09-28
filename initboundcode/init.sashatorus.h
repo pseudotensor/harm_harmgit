d00e14aeeed0e2eba7273879cbcef07b0a902cc6
Alexander Tchekhovskoy <atchekho@princeton.edu>
2012-09-28 10:33:35 -0400

    16x16x32->64x64x1

--- initboundcode/init.sashatorus.h
+++ initboundcode/init.sashatorus.h
@@ -12,9 +12,9 @@
 #endif
 
 // AKMARK: # of cells per tile (N1, N2, N3 = r, theta, phi)
-#define N1 16
-#define N2 16
-#define N3 32
+#define N1 64
+#define N2 64
+#define N3 1
 
 // atch adjusts
 #undef MAXWELL
