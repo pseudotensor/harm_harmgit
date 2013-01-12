81edc0442fe191f5a973424734aa8383dee7ce82
Jonathan McKinney on ki-jmck <jmckinne@slac.stanford.edu>
2013-01-12 07:46:35 -0800

    more bug fixes

--- vchar.c
+++ vchar.c
@@ -179,11 +179,6 @@ int vchar(FTYPE *pr, struct of_state *q, int dir, struct of_geom *geom, FTYPE *v
 
 
 
-  if(EOMRADTYPE!=EOMRADNONE){
-    // KORALTODO -- compute radiation wave speed and insert as second type of wave speed need to store, since need both types of speeds.
-  }
-
-
 
 
 #if(BOUNDARYSMOOTHER)
