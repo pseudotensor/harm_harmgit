ca7a11a4924ebf8987ce95a3e1a05f79e07e5fa3
Alexander Tchekhovskoy <atchekho@berkeley.edu>
2013-12-12 23:04:33 -0800

    added scripts for automating tests

---
+++ doradwavetests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+for i in 1001
+do
+    echo "Doing test #${i}..."
+    for n in 32 64 #128 256 512
+    do
+        cd ~/Research/code/harm
+        dirname=tests/radwave${i}_${n}
+        mkdir -p tests
+        mkdir -p $dirname
+	#choose test
+	cat initboundcode/init.koral.c | sed "s/^\([ \t]*\)RADWAVE_NUMERO=[0-9]*\;/\1RADWAVE_NUMERO=${i}\;/g" > $dirname/init.koral.c
+        cp $dirname/init.koral.c initboundcode/init.koral.c
+	#choose resolution
+        cat initboundcode/init.koral.h | sed "s/N1 [0-9 ]*\/\/RADWAVE/N1 $n   \/\/RADWAVE/g" > $dirname/init.koral.h
+        cp $dirname/init.koral.h initboundcode/init.koral.h
+        echo "Compiling test #${i} using ${n} cells: radwave${i}_${n} ..."
+        make superclean &> $dirname/compile_log.txt
+        make prep &> $dirname/compile_log.txt
+        make -j 4 &> $dirname/compile_log.txt
+        cp grmhd $dirname
+        cd $dirname
+        echo "Running test #${i} using ${n} cells: radwave${i}_${n} ..."
+        ./grmhd 1 1 1  &> run_log.txt
+     done
+done
+echo "Done!"
