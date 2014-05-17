b9611e92be675769e3a9c225d819d59d9a3038c8
Jonathan C. McKinney <pseudotensor@gmail.com>
2014-05-17 17:47:07 -0400

    docs10

--- defs.liaison.h
+++ defs.liaison.h
@@ -1,3 +1,9 @@
+
+/*! \file defs.liaison.h
+    \brief For liaison.c code to bind Broderick ray-tracing code and HARM
+    //
+    */
+
 #include "global.liaison.h"
 #include "superdefs.liaison.h"
 #include "defs.general.h"
