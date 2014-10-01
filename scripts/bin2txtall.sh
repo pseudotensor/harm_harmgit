223b3c0e64f426d1d3305425a09de513a1bdd44c
Jonathan C. McKinney <pseudotensor@gmail.com>
2014-10-01 11:27:45 -0400

    script

---
+++ scripts/bin2txtall.sh
@@ -0,0 +1,5 @@
+bin2txt 1 2 0 -1 3 256 128 64 1 gdump.bin gdump d 126
+bin2txt 1 2 0 -1 3 256 128 64 1 dump2425.bin dump2425 d 61
+bin2txt 1 2 0 -1 3 256 128 64 1 raddump2425.bin raddump2425 d 44
+bin2txt 1 2 0 -1 3 256 128 64 1 dissmeasuredump2425.bin dissmeasuredump2425 d 13
+bin2txt 1 2 0 -1 3 256 128 64 1 eosdump2425.bin eosdump2425 d 54
