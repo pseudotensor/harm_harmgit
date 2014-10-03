d9f35cd03ff79f8509e1167a9a9f97be49456aad
Jonathan C. McKinney <pseudotensor@gmail.com>
2014-10-03 19:34:33 -0400

    docs

--- scripts/bin2txtall.sh
+++ scripts/bin2txtall.sh
@@ -3,3 +3,38 @@ bin2txt 1 2 0 -1 3 256 128 64 1 dump2425.bin dump2425 d 61
 bin2txt 1 2 0 -1 3 256 128 64 1 raddump2425.bin raddump2425 d 44
 bin2txt 1 2 0 -1 3 256 128 64 1 dissmeasuredump2425.bin dissmeasuredump2425 d 13
 bin2txt 1 2 0 -1 3 256 128 64 1 eosdump2425.bin eosdump2425 d 54
+
+
+bin2txt 1 2 0 -1 3 256 128 64 1 gdump.bin gdump d 126
+bin2txt 1 2 0 -1 3 256 128 64 1 dump0999.bin dump0999 d 61
+bin2txt 1 2 0 -1 3 256 128 64 1 raddump0999.bin raddump0999 d 44
+bin2txt 1 2 0 -1 3 256 128 64 1 dissmeasuredump0999.bin dissmeasuredump0999 d 13
+bin2txt 1 2 0 -1 3 256 128 64 1 eosdump0999.bin eosdump0999 d 54
+bin2txt 1 2 0 -1 3 256 128 64 1 failfloordudump0999.bin failfloordudump0999 d 13
+bin2txt 1 2 0 -1 3 256 128 64 1 debug0999.bin debug0999 d 128
+ln -s debug0999 debugdump0999
+
+bin2txt 1 2 0 -1 3 256 128 64 1 dump0492.bin dump0492 d 61
+bin2txt 1 2 0 -1 3 256 128 64 1 raddump0492.bin raddump0492 d 44
+bin2txt 1 2 0 -1 3 256 128 64 1 dissmeasuredump0492.bin dissmeasuredump0492 d 13
+bin2txt 1 2 0 -1 3 256 128 64 1 eosdump0492.bin eosdump0492 d 54
+bin2txt 1 2 0 -1 3 256 128 64 1 failfloordudump0492.bin failfloordudump0492 d 13
+bin2txt 1 2 0 -1 3 256 128 64 1 debug0492.bin debug0492 d 128
+ln -s debug0492 debugdump0492
+
+bin2txt 1 2 0 -1 3 256 128 64 1 dump0400.bin dump0400 d 61
+bin2txt 1 2 0 -1 3 256 128 64 1 raddump0400.bin raddump0400 d 44
+bin2txt 1 2 0 -1 3 256 128 64 1 dissmeasuredump0400.bin dissmeasuredump0400 d 13
+bin2txt 1 2 0 -1 3 256 128 64 1 eosdump0400.bin eosdump0400 d 54
+bin2txt 1 2 0 -1 3 256 128 64 1 failfloordudump0400.bin failfloordudump0400 d 13
+bin2txt 1 2 0 -1 3 256 128 64 1 debug0400.bin debug0400 d 128
+ln -s debug0400 debugdump0400
+
+
+bin2txt 1 2 0 -1 3 256 128 64 1 dump0311.bin dump0311 d 61
+bin2txt 1 2 0 -1 3 256 128 64 1 raddump0311.bin raddump0311 d 44
+bin2txt 1 2 0 -1 3 256 128 64 1 dissmeasuredump0311.bin dissmeasuredump0311 d 13
+bin2txt 1 2 0 -1 3 256 128 64 1 eosdump0311.bin eosdump0311 d 54
+bin2txt 1 2 0 -1 3 256 128 64 1 failfloordudump0311.bin failfloordudump0311 d 13
+bin2txt 1 2 0 -1 3 256 128 64 1 debug0311.bin debug0311 d 128
+ln -s debug0311 debugdump0311
