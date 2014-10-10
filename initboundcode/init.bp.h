68463563c017d658547def5eff19d17a694e274c
Mark James Avara <mavara@pfe21.nas.nasa.gov>
2014-10-09 17:26:05 -0700

    production start mad mid res no tilt

--- initboundcode/init.bp.h
+++ initboundcode/init.bp.h
@@ -16,7 +16,7 @@
 
 #define N1 12 //12 //12 //32  // MARKTODO resolution
 #define N2 8 //8  //6 //24
-#define N3 10 //10 // 6 for big runs //8 //6 //8   //16
+#define N3 8 //10 // 6 for big runs //8 //6 //8   //16
 
 
 // atch adjusts
