f7e89a18d47db5f7690f90d579667f7d2a807660
danilomorales <danilomorales.astro@gmail.com>
2016-04-03 13:17:03 -0400

    changed magnetic field

--- initboundcode/init.koral.h
+++ initboundcode/init.koral.h
@@ -1083,9 +1083,9 @@ struct Ccoordparams {
 // N1=30 if using log coords from r=1.7 to r=50
 // N1=60 if using 1.5*hor - 40 (or 27.8)
 // N1=70 if using 1.5*hor - 30 (or 27.8)
-#define N1 16 
-#define N2 16
-#define N3 8
+#define N1 8 
+#define N2 8
+#define N3 16 
 
    //#define N1 128
 //#define N2 64
