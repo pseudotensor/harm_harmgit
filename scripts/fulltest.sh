2e253adb539178c2098cfae23c5ea70de8977f0d
Jonathan C. McKinney <pseudotensor@gmail.com>
2017-01-27 23:57:32 -0500

    fulltest update

--- scripts/fulltest.sh
+++ scripts/fulltest.sh
@@ -1,8 +1,20 @@
+#!/bin/bash
+
+########
+### Code to run through complete end-to-end test of harm or harmrad
+########
+
+
+
+########
+# assume already done this outside this script:
 # git clone git@github.com:pseudotensor/harm_harmgit.git
 # cd harm_harmgit/
 
-# get version setup for auto for openmp
-git branch auto
+########
+# now can script actions
+# get version setup for master
+git branch master
 
 # default for master branch:
 # for non-radiation problem
@@ -15,11 +27,13 @@ ln -s initboundcode/init.fishmon.c init.c ; ln -s initboundcode/init.fishmon.h i
 make superclean ; make prep ; make -j 16
 
 # default for master branch:
-mkdir run ; cd run ; cp ../grmhd . ; ./grmhd 32 1 1 1 # mpi and openmp both on
+#mkdir run ; cd run ; cp ../grmhd . ; ./grmhd 1 1 1 1 # mpi and openmp both on
+mkdir run ; cd run ; cp ../grmhd . ; nohup mpirun -np 16 ./grmhd 1 4 4 1 # mpi and openmp both on
 
 # default for koralinsert or auto branches
 # run with 32 openmp threads (efficient use of all 16 cores on 1 node)
-#mkdir run ; cd run ; cp ../grmhd . ; nohup mpirun -np 1 ./grmhd 32 1 1 1 &
+#mkdir run ; cd run ; cp ../grmhd . ; nohup ./grmhd 32 1 1 1 &
+#mkdir run ; cd run ; cp ../grmhd . ; nohup mpirun -np 1 ./grmhd 1 1 1 1 &
 
 # run for about an hour to get at least a few files.
 # this doesn't end the job, just pauses for 1 hour to get to analysis
@@ -83,22 +97,30 @@ sleep 1h
 mv run mhd1
 
 # ensure you have python utilities
-# cd ~ ; git clone git@github.com:pseudotensor/harm_pythontools.git ; mv harm_pythontools py ; cd py ; git branch jon
+mypath=`pwd`
+DIRECTORY=~/py/
+if [ -d "$DIRECTORY" ]; then
+    echo "directory $DIRECTORY already exists, not overwritting"
+else
+    echo "directory $DIRECTORY does not exist, creating"
+    cd ~ ; git clone git@github.com:pseudotensor/harm_pythontools.git ; mv harm_pythontools py ; cd py ; git branch jon
+    cd $mypath
+fi
 
-# cp ~/py/scripts/makeallmovie.sh .
+cp ~/py/scripts/makeallmovie.sh .
 # edit makeallmovie.sh and change line with dircollect="..." and dirruns="..." so that "..." is "mhd1"
 # edit makeallmovie.sh and set numkeep to 50 for test
-
+cat makeallmovie.sh | sed 's/^dirruns=.*/dirruns=\"mhd1\"/g' | sed 's/^numkeep=.*/numkeep=50/g' > makeallmovielocal.sh
 
 # assuming on single node type job (default if system not detected), then do:
 # setup links, copy files, and makeavg step that makes avg2d_*.npy.
 # then merges to avg2d.npy (average used for rest of analysis)
 export moviename="movie1"
-bash ./makeallmovie.sh ${moviename} 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 0
+bash ./makeallmovielocal.sh ${moviename} 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 0
 # check if mhd1/$moviename/avg2d.npy created
 
 # do rest of analysis
-bash ./makeallmovie.sh ${moviename} 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 0 0 1 1
+bash ./makeallmovielocal.sh ${moviename} 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 0 0 1 1
 # should generate analysis results in mhd1/$moviename/*.png *.txt etc.
 
 # can make movie by doing (new2 for avconv and new3 for ffmpeg)
@@ -109,7 +131,7 @@ bash ./makelinkimagenew2.sh
 # can use smplayer or other things to view mp4's
 
 
-# see "system" in makeallmovie.sh and how used in ~/py/scripts/makemovie.sh to see what kinds of systems already setup and how you can setup your own system by example.
+# see "system" in makeallmovielocal.sh and how used in ~/py/scripts/makemovie.sh to see what kinds of systems already setup and how you can setup your own system by example.
 
 # common issues on single node systems:
 # 1) dash incomplete, need to use bash to run scripts.  Best to remove dash in favor of bash
